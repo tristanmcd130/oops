e0a8688645522be9c87071fe36877a986af43dd2
tristanmcd130 <tswaryco@duck.com>
2025-06-27 18:23:30 -0400

    profiling

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open Oops
 
-let vm = Vm.make ([] |> List.to_seq |> Hashtbl.of_seq)
+let vm = Vm.make ([("print", Value.Primitive (fun [x] -> x |> Value.to_string |> print_endline; Null))] |> List.to_seq |> Hashtbl.of_seq)
 let run_from_channel channel =
   let chunk = Chunk.empty () in
   channel |> Lexing.from_channel |> Parser.program Lexer.read |> Chunk.compile chunk (Scope.make None false []);
